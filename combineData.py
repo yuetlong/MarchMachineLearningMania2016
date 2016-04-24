28b3b2613673ccb5d79bad28dce03f7929e0d836
pat-son <patrick.son@comcast.net>
2016-04-24 16:47:41 -0400

    Wloc changed to numeric

--- combineData.py
+++ combineData.py
@@ -9,7 +9,6 @@ combined = pd.concat(frames)
 combined = combined[combined.Season >= 2012]
 combined = combined[combined.Season <= 2015]
 
-combined = combined.drop(['Daynum', 'Wloc', 'Numot', 'Wteam', 'Lteam', 'Season'], 1)
 combined['score'] = combined.Wscore - combined.Lscore
 combined['fgm'] = combined.Wfgm - combined.Lfgm
 combined['fga'] = combined.Wfga - combined.Lfga
@@ -24,6 +23,20 @@ combined['to'] = combined.Wto - combined.Lto
 combined['stl'] = combined.Wstl - combined.Lstl
 combined['blk'] = combined.Wblk - combined.Lblk
 combined['pf'] = combined.Wpf - combined.Lpf
-combined = combined.drop(['Wscore', 'Lscore', 'Wfgm', 'Lfgm', 'Wfga', 'Lfga', 'Wfgm3', 'Lfgm3', 'Wfga3', 'Lfga3', 'Wftm', 'Lftm', 'Wfta', 'Lfta', 'Wor', 'Lor', 'Wdr', 'Ldr', 'Wast', 'Last', 'Wto', 'Lto', 'Wstl', 'Lstl', 'Wblk', 'Lblk', 'Wpf', 'Lpf'], 1)
+
+numWins = {}
+for i in range(1101, 1465):
+  numWins[i] = combined[combined.Wteam == i].Wteam.count()
+
+home = combined[combined.Wloc == 'H']
+home['Wloc'] = 1
+neutral = combined[combined.Wloc == 'N']
+neutral['Wloc'] = .5
+away = combined[combined.Wloc == 'A']
+away['Wloc'] = 0
+courts = [home, neutral, away]
+combined = pd.concat(courts)
+
+combined = combined.drop(['Daynum', 'Numot', 'Wteam', 'Lteam', 'Season', 'Wscore', 'Lscore', 'Wfgm', 'Lfgm', 'Wfga', 'Lfga', 'Wfgm3', 'Lfgm3', 'Wfga3', 'Lfga3', 'Wftm', 'Lftm', 'Wfta', 'Lfta', 'Wor', 'Lor', 'Wdr', 'Ldr', 'Wast', 'Last', 'Wto', 'Lto', 'Wstl', 'Lstl', 'Wblk', 'Lblk', 'Wpf', 'Lpf'], 1)
 
 combined.to_csv('combinedData.csv', index=False)
