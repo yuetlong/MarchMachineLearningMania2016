91eb069ed282ba9183887dc5ec580f676d1ee2f5
Farzeen Harunani <farzeen@outlook.com>
2016-04-24 18:56:25 -0400

    Elo python script now outputs win probabilities

--- elo/eloRating.py
+++ elo/eloRating.py
@@ -4,6 +4,7 @@ import numpy as np
 
 data = pd.read_csv('../data/RegularSeasonCompactResults.csv')
 teams = pd.read_csv('../data/Teams.csv'); 
+sample = pd.read_csv('../data/SampleSubmission.csv')
 
 # Initialize Elo Variables
 homeAdvantage = 100; 
@@ -31,9 +32,14 @@ for index, row in data.iterrows():
 	ratings[data.Wteam[index]] = Ra + k*(1-Ea)
 	ratings[data.Lteam[index]] = Rb + k*(0-Eb)
 
-#print(ratings)
-pd.DataFrame.from_dict(ratings,orient='index').to_csv('eloResults.csv', index=False)
+games = pd.concat([sample['Id'],sample['Id'].str.split('_', expand=True)], axis=1)
+games.rename(columns={0: 'season', 1: 'team1',2: 'team2'}, inplace=True)
+games['season'] = pd.to_numeric(games['season'])
+games['team1'] = pd.to_numeric(games['team1'])
+games['team2'] = pd.to_numeric(games['team2'])
+games['Pred'] = 0
+for i in range(0,len(games.index)): 
+	m = ratings[games.team2[i]] - ratings[games.team1[i]]
+	games.ix[i,'Pred'] = 1/(1+(10**(m/200)))
 
-
-
-
+games[['Id','Pred']].to_csv('eloResults.csv',index=False)
